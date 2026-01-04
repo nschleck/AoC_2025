7fca57d425da6ab0436ccdef8d045ae14e9cfeb0
Nick Schleck <38449492+nschleck@users.noreply.github.com>
2026-01-03 16:47:23 -0800

    day7 final

--- day7.py
+++ day7.py
@@ -1,14 +1,25 @@
+import random
 with open("day7_input.txt", 'r') as file:
     row_list = []
     for line in file:
         row_list.append(line.rstrip('\n'))
 
 
-count = 0
 row_str = row_list[0]
 next_row_str = row_list[1]
 
-print(row_str)
+# create paths list and a blank path template
+paths_list = []
+paths_row = []
+for i in range(len(row_str)):
+    paths_row.append(0)
+for i, char in enumerate(row_str):
+    if char == "S":
+        paths_row[i] = 1
+
+next_paths_row = paths_row.copy()
+
+# print(f"{row_str}   {paths_row}")
 
 for row in range(1, len(row_list)):
     for col, char in enumerate(row_str):
@@ -16,19 +27,28 @@ for row in range(1, len(row_list)):
             next_char = next_row_str[col]
             if next_char == "^":
                 next_row_str = next_row_str[:col-1] + "|^|" + next_row_str[col+2:]  
-                count += 1  
+                next_paths_row[col-1] += paths_row[col]
+                next_paths_row[col+1] += paths_row[col]
+                next_paths_row[col] = 0
             else:
                 # insert a "|" char below
                 next_row_str = next_row_str[:col] + "|" + next_row_str[col+1:]    
+                # next_paths_row[col] += paths_row[col] #max(paths_row[col],1)
             # print(f"^ found at row {row}, column {col}")
     
-    print(f"{next_row_str}   [row {row}]")
+    #sum up paths
+    sum = 0
+    for item in next_paths_row:
+        sum+=item
+
+    print(f"{row_str}   {next_paths_row} [row {row}] [{sum} paths]")
+
     row_str = next_row_str
+    paths_row = next_paths_row.copy()
     try:
         next_row_str = row_list[row+1]
     except IndexError:
         pass
 
-#print(next_row_str)
+print(f"{row_str}   {paths_row} [row {row}]")
 
-print(count)
